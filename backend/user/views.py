3d061a9f5738d39f26ce103f874128fe1315b793
Alexander <amarjin6@gmail.com>
2022-09-21 13:20:03 +0300

    Resolve issue with permissions

--- backend/user/views.py
+++ backend/user/views.py
@@ -4,12 +4,11 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from user.models import User
 from user.serializers import UserSerializer, CreateUserSerializer
-from core.mixins.permissions import PermissionMixin
 from core.mixins.serializers import DynamicSerializerMixin
 
 
-class UserViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet,
-                  PermissionMixin, DynamicSerializerMixin):
+class UserViewSet(DynamicSerializerMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
@@ -19,3 +18,9 @@ class UserViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.Lis
     serializer_action_classes = {
         'create': CreateUserSerializer,
     }
+
+    def get_permissions(self):
+        for actions, permission in self.permissions_mapping.items():
+            if self.action in actions:
+                self.permission_classes = (permission,)
+        return super(self.__class__, self).get_permissions()
