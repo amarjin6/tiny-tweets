c31355e663ca407a2740ea0244f99a903c65e976
Alexander <amarjin6@gmail.com>
2022-09-26 12:51:59 +0300

    Create follow/unfollow func & smtp email sender

--- page/models.py
+++ page/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 from user.models import User
@@ -11,14 +12,14 @@ class Tag(models.Model):
 
 
 class Page(models.Model):
-    uuid = models.CharField(max_length=30, unique=True)
+    uuid = models.CharField(unique=True, editable=False, default=uuid.uuid4)
     title = models.CharField(max_length=80)
     tags = models.ManyToManyField(Tag, related_name='tags_page')
     image = models.URLField(null=True, blank=True)
     description = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_page')
-    followers = models.ManyToManyField(User, related_name='followers_page')
-    follow_requests = models.ManyToManyField(User, related_name='follow_requests_page')
+    followers = models.ManyToManyField(User, related_name='followers_page', blank=True)
+    follow_requests = models.ManyToManyField(User, related_name='follow_requests_page', blank=True)
     is_private = models.BooleanField(default=False)
     is_blocked = models.BooleanField(default=False)
     unblock_date = models.DateTimeField(null=True, blank=True)
@@ -30,6 +31,8 @@ class Page(models.Model):
 class Post(models.Model):
     page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='page_post')
     content = models.CharField(max_length=180)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_post')
+    liked_by = models.ManyToManyField(User, related_name='liked_by_post', blank=True)
     reply_to = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True, blank=True, related_name='reply_to_post')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
