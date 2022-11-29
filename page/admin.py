ea63594a6f01b8cf8c15cb49098e0c3e681a3fd7
Alexander <amarjin6@gmail.com>
2022-11-29 23:44:25 +0300

    Pages init

--- page/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from page.models import Tag, Page, Post
-
-admin.site.register(Tag)
-admin.site.register(Page)
-admin.site.register(Post)
--- page/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from core.services import AWSManager
-
-
-@shared_task()
-def send_mail(data: list) -> dict:
-    return AWSManager.send_mail(data)
